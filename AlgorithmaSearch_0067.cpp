a171674aa1c7655903252857d4998a7d2ae3d35e
Naufal0067 <naufalopang1928@gmail.com>
2024-04-05 14:49:17 +0700

    memasukkan do

--- AlgorithmaSearch_0067.cpp
+++ AlgorithmaSearch_0067.cpp
@@ -33,4 +33,15 @@ void LinearSearch()
     char ch;
     int ctr; // Number of comparisons
     int item;
+
+    do
+    {
+        // Accept the number to be searched 
+        cout << "\nEnter the element you want to search: "; // step 1
+        cin >> item;
+
+        ctr = 0;
+        i = 0; // step 2
+        while 
+    }
 }
